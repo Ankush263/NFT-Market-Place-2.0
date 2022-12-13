298ea1910c082cd8ab885aaa3dcaac0a9a6b87ae
Ankush263 <ankushbanik263@gmail.com>
2022-12-13 11:06:34 +0530

    update

--- client/components/Navbar/NavBar.jsx
+++ client/components/Navbar/NavBar.jsx
@@ -1,5 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Style from './NavBar.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { MdNotifications } from 'react-icons/md';
+import { BsSearch } from 'react-icons/bs';
+import { CgMenuLeft, CgMenuRight } from 'react-icons/cg';
+import { Discover, HelpCenter, Profile, Sidebar } from './index';
+import { Button } from '../componentsindex';
+
 
 function NavBar() {
   return (
