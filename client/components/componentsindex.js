a3f1ce8d2aaf0663a7a67fb7b8b70a017899b24f
Ankush263 <ankushbanik263@gmail.com>
2022-12-14 11:05:36 +0530

    Add components

--- client/components/componentsindex.js
+++ client/components/componentsindex.js
@@ -1,3 +1,9 @@
 import Button from './Button/Button';
 import Footer from './Footer/Footer';
 import NavBar from './Navbar/NavBar';
+import HeroSection from './HeroSection/HeroSection';
+import Service from './Service/Service';
+import BigNFTSilder from './BigNFTSilder/BigNFTSilder';
+import Subscribe from './Subscribe/Subscribe';
+
+export { Button, Footer, NavBar, HeroSection, Service, BigNFTSilder, Subscribe };
