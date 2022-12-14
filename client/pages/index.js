de8f5ff0880c6bb7b4d29623a2008ada38fe9cec
Ankush263 <ankushbanik263@gmail.com>
2022-12-15 00:09:32 +0530

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import Style from "../styles/index.module.css";
-import { HeroSection, Service, BigNFTSilder, Subscribe } from '../components/componentsindex';
+import 
+{ 
+  HeroSection, 
+  Service, 
+  BigNFTSilder, 
+  Subscribe,
+  Title,
+  Category,
+  Filter,
+  NFTCard,
+  Collection,
+  FollowerTab,
+  AudioLive,
+  Slider,
+  Brand,
+  Video
+} 
+from '../components/componentsindex';
 
 function index() {
   return (
@@ -8,7 +25,32 @@ function index() {
       <HeroSection />
       <Service />
       <BigNFTSilder />
+      <Title
+        heading="Audio Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <AudioLive />
+      <FollowerTab />
+      <Slider />
+      <Collection />
+      {/* <Title
+        heading="New Collection"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      /> */}
+      <Title
+        heading="Featured NFTs"
+        paragraph="Discover the most outstanding NFTs in all topics of life."
+      />
+      <Filter />
+      <NFTCard />
+      <Title
+        heading="Browse by category"
+        paragraph="Explore the NFTs in the most featured categories."
+      />
+      <Category />
       <Subscribe />
+      <Brand />
+      <Video />
     </div>
   )
 }
